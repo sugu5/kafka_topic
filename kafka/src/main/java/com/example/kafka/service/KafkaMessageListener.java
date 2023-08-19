5d9c8c2ee270852cf18bfc53c0c367269899b8e1
sugu5 <64516302+sugu5@users.noreply.github.com>
2023-08-19 19:50:57 +0530

    Add files via upload

---
+++ kafka/src/main/java/com/example/kafka/service/KafkaMessageListener.java
@@ -0,0 +1,19 @@
+package com.example.kafka.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "java",groupId = "g1")
+    public void consume(String message)
+    {
+        logger.info("consumer consume the message {}",message);
+
+    }
+}
